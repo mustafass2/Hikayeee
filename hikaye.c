52daeaa54f7e8d0ab1f72044e2ac8c1935719392
mustafass2 <mustafasaput93@gmail.com>
2024-12-19 15:56:00 +0300

    Update hikaye.c

--- hikaye.c
+++ hikaye.c
@@ -6,5 +6,6 @@ int main() {
 printf("Soğuk bir kış akşamıydı\nKar giderek şiddetleniyordu. 2 kardeş bir an önce kafalarını bir yere sokmak yer arıyorlardı.\n"); //Burak Gökmen hikaye başlangıcı
 printf("Son çıkan efsanelerden sonra bu ormanın derinliklerinde güvende olmadıklarını biliyorlardı..");//Burak Gökmen hikaye başlangıcı
 printf("Kardeşlerden küçük olanı ormanın derinliklerinden gelen bir ses duyduğunu söyledi..")
+printf("iki kardeş duydukları sese doğru gitmeye karar verdiler ve sese doğru gitmeye başladılar..");//Mustafa Saput
 
 }
